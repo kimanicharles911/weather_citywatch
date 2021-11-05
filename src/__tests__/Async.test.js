3b9af8e6c5caef32182c543e794660c72e5a924b
kimanicharles911 <kimanicharles911@gmail.com>
2021-11-05 16:12:26 +0300

    finshed fetch API test and commenting

--- src/__tests__/Async.test.js
+++ src/__tests__/Async.test.js
@@ -1,17 +1,24 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import App from "../App.jsx";
-import FormInputComponent from "../components/FormInputComponent.jsx";
-import ResultsCardComponent from "../components/ResultsCardComponent.jsx";
+/* I created an object that stores the api key and base URL of the weather APi */
+const api = {
+  key: process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY,
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+/* I stored the city whose weather data I want to fetch*/
+const city = "Nairobi";
 
 describe("Async fetch tests", () => {
-  it('check if heading is rendered', () => {
-    render(<ResultsCardComponent cityProp='Meru' />)
-    expect(screen.getByText(/Results/i)).toBeVisible()
-  });
-
-  it("check if a list item is rendered", async () => {
-    render(<ResultsCardComponent cityProp='Meru'  />);
-    const tempSpanEl = await screen.findByTestId("temp-value-span");
-    expect(tempSpanEl).toBeInTheDocument();
+  it('fetch api test', async() => {
+    const request = await fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+    const response = await request.json();
+    expect(response.sys.country).toBe("KE");
   });
 });
+
+/* 
+I defined a describe block which is used to describe multiple  tests that belong to one group.
+I define a test block using it. The first paramter of the test block is the test's description and the last parameter is a callback function that executes the test.
+I fetch the wether data and store it in a variable called request.
+I then convert the JSON in the request variable and store it in the response variable.
+I then use the expect assertion to check the country code of the city returned whether it matches it's country's code using the the toBe matcher.
+*/
--- src/__tests__/FormInputComponent.test.js
+++ src/__tests__/FormInputComponent.test.js
@@ -8,7 +8,12 @@ it("renders FormInputComponent correctly", () => {
   expect(tree).toMatchSnapshot();
 });
 
-/* REFERENCES
+/* 
+I imported renderer to enable test renders of components to be done while generating snapshots.
+I imported the FormInputComponent component to be tested.
+I define a test block using it. The first paramter of the test block is the test's description and the last parameter is a callback function that executes the snapshot test.
+
+  REFERENCES
   ===========> 
   * I learnt to make snapshots from https://jestjs.io/docs/snapshot-testing
   */
